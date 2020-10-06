0da5179b5f74f5e7362119e4a26766794113b41e
mattcbodily <mattcbodily@gmail.com>
2020-10-06 16:32:57 -0600

    users can now add product data and view savings data

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 def calculate_savings():
     if request.method == 'POST':
         product_name = request.form['product']
-        product_price = request.form['price']
-        product_qty = request.form['quantity']
+        product_price = float(request.form['price'])
+        product_qty = int(request.form['quantity'])
+        weekly_savings = product_price * product_qty * 7
+        monthly_savings = product_price * product_qty * 30
+        yearly_savings = product_price * product_qty * 365
 
         print(product_name)
         print(product_price)
         print(product_qty)
 
-    return render_template('landing.html')
+    return render_template('landing.html', weekly_savings=weekly_savings, monthly_savings=monthly_savings, yearly_savings=yearly_savings)
